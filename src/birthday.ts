ca66ad46efa71686f9b127c101ff7ddd77b31ae7
J. Cohen <hostelmarketing@gmail.com>
2021-12-20 16:03:13 -0800

    Fix typo

--- src/birthday.ts
+++ src/birthday.ts
@@ -1,7 +1,7 @@
 import { Hobbit } from "./hobbit";
 
 export function haveBirthday(hobbit: Hobbit): Hobbit {
-    console.log(`======= celebrating ${hobbit}'s birthday =======`);
+    console.log(`======= celebrating ${hobbit.name}'s birthday =======`);
     return {
         ...hobbit,
         age: hobbit.age + 1,
